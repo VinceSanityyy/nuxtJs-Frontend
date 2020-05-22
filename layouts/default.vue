f88dd8b7ac9a1e08c2f298dbe16dc9d725c6f2de
vincesanityyy <vbustillo97@gmail.com>
2020-05-22 23:53:17 +0800

    created new nuxt changed Ui to buefy

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Navbar></Navbar>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Navbar from '~/components/Navbar.vue'
+export default {
+  components:{
+    Navbar,
+  },
+  data () {
+    return {
+     
+    }
+  }
+}
+</script>
